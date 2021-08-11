6fc27ece8b891b4ac22712f64076fe6559450d2a
zahid33us <zahidus33@gmail.com>
2021-08-10 20:31:02 -0700

    project2

---
+++ src/learningArray/Array.java
@@ -0,0 +1,18 @@
+package learningArray;
+
+public class Array {
+	public static void main(String[] args) {
+		String[] you=new String[3];
+				you[0]="Love yourself";
+				you[1]="Live is awesome";
+				you[2]="Live    present";
+				
+				for (int i=0; i<you.length; i++) {
+					System.out.println(you[i]);
+				}
+
+
+	}
+}
+
+
---
+++ src/learningJava/PracticeJave.java
@@ -0,0 +1,13 @@
+package learningJava;
+
+public class PracticeJave {
+
+	public static void main(String[] args) {
+		String name = "Zahid";
+		int age = 36;
+		double annual_salary=10000.00;
+		System.out.println("My name is " + name +  ". My age is " + age + ". My salary is " + annual_salary);
+
+	}
+
+}
